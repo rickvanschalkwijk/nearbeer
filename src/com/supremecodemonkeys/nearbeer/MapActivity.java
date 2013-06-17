6da15190c41dfc75b67225ddceaad3f6316f8a92
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-06-17 21:55:06 +0200

    added google maps

--- src/com/supremecodemonkeys/nearbeer/MapActivity.java
+++ src/com/supremecodemonkeys/nearbeer/MapActivity.java
@@ -1,23 +1,68 @@
 package com.supremecodemonkeys.nearbeer;
 
+import android.annotation.TargetApi;
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.view.Menu;
 
-public class MapActivity extends FragmentActivity {
-
+public class MapActivity extends Activity {
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_map);
+		setupActionBar();
+		//mMap = ( (MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
 	}
+	
 
+	@Override
+	protected void onStart() {
+		super.onStart();
+	}
+	
+	@Override
+	protected void onResume() {
+		super.onResume();
+	}
+	
+	@Override
+	protected void onPause() {
+		super.onPause();
+	}
+	
+	@Override
+	protected void onStop() {
+		super.onStop();
+	}
+	
+	@Override
+	protected void onRestart(){
+		super.onRestart();
+	}
+	
+	@Override
+	protected void onDestroy(){
+		super.onDestroy();
+	}
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.map, menu);
 		return true;
+	}
+	
+	/**
+	 * Set up the {@link android.app.ActionBar}, if the API is available.
+	 */
+	@TargetApi(Build.VERSION_CODES.HONEYCOMB)
+	private void setupActionBar() {
+		if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB) {
+			getActionBar().setDisplayHomeAsUpEnabled(true);
+		}
 	}
 
 }
