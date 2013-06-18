892cc9cf563e0d59cf2a94e4b989e7c145dbd643
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-06-18 11:56:55 +0200

    added list model

---
+++ src/com/supremecodemonkeys/core/GooglePlaces.java
@@ -0,0 +1,12 @@
+package com.supremecodemonkeys.core;
+
+import org.apache.http.io.HttpTransportMetrics;
+import org.apache.http.client.HttpResponseException;
+
+import com.google.api.client.googleapis.GoogleHeaders;
+
+public class GooglePlaces {
+	
+	private static final HttpTransport HTTP_TRANSPORT = new NetHttpTransport();
+
+}
---
+++ src/com/supremecodemonkeys/models/PlacesList.java
@@ -0,0 +1,9 @@
+package com.supremecodemonkeys.models;
+
+import java.io.Serializable;
+import java.util.List;
+
+public class PlacesList implements Serializable {
+	public String status;
+	public List<Place> results;
+}
