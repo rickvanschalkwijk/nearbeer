cd63fa45540e1c0c233585a1179856c06f019d16
rickvschalkwijk <rickvschalkwijk@gmail.com>
2013-06-18 11:31:00 +0200

    added place detail model

---
+++ src/com/supremecodemonkeys/models/PlaceDetails.java
@@ -0,0 +1,16 @@
+package com.supremecodemonkeys.models;
+
+import java.io.Serializable;
+
+public class PlaceDetails  implements Serializable{
+	public String status;
+	public Place result;
+	
+	@Override
+	public String toString(){
+		if(result != null){
+			return result.toString();
+		}
+		return super.toString();
+	}
+}
